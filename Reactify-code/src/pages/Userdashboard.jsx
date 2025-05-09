ff039f6fce23230e815dbef4d4bb6fd91d0f7cf0
akash revankar <dexterArz>
2025-05-09 17:49:40 +0530

    fixed css in dashboard and file

--- Reactify-code/src/pages/Userdashboard.jsx
+++ Reactify-code/src/pages/Userdashboard.jsx
@@ -30,7 +30,7 @@ const Userdashboard = () => {
             </button>
           </div>
           <div className="folder">
-            {[...Array(2)].map((_, index) => (
+            {[...Array(10)].map((_, index) => (
               <File key={index} />
             ))}
           </div>
